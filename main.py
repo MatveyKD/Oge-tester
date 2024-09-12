f0687603014b22a820d95f39f0e953a3cb1369e8
MatveyKD <97842332+MatveyKD@users.noreply.github.com>
2024-09-12 16:03:38 +0300

    Add pandas DB (delete json usage)

--- main.py
+++ main.py
@@ -19,8 +19,19 @@ from telegram.ext import (
     PreCheckoutQueryHandler,
     ConversationHandler
 )
+import pandas as pd
 
 from environs import Env
+
+questions_path = "C:/Users/Honor/Documents/GitHub/Oge-tester/Questions.xlsx"
+vars_path = "C:/Users/Honor/Documents/GitHub/Oge-tester/Vars.xlsx"
+passings_path = "C:/Users/Honor/Documents/GitHub/Oge-tester/Passings.xlsx"
+
+# # открываем файлы в качестве БД:
+# df_questions = pd.read_excel(questions_path, index_col=None).reset_index(drop=True)
+# df_vars = pd.read_excel(vars_path, index_col=None).reset_index(drop=True)
+# df_passings = pd.read_excel(passings_path, index_col=None).reset_index(drop=True)
+
 
 env = Env()
 env.read_env()
@@ -31,10 +42,6 @@ def start_conversation(update, context):
     user_id = update.effective_user.id
     # print(update)
     username = update.effective_user.username
-
-    data = {}
-    with open("users.json", encoding="UTF-8") as file:
-        data = json.load(file)
 
     # if username in data["admins"]:
     keyboard = [
@@ -95,12 +102,18 @@ def generate(update, context, quest):
         else:
             uniq_id += r.choice(letters)
 
-    with open("users.json", "r+", encoding="UTF-8") as file:
-        data = json.load(file)
-        data["vars"][uniq_id] = {"quest": quest, "passings": {}}
-        file.seek(0)
-        file.write(json.dumps(data))
-        # file.truncate(0)
+    df_vars = pd.read_excel(vars_path, index_col=None).reset_index(drop=True)
+
+    # записать новый вариант в файл Vars.xlxs
+    new_var_code = uniq_id
+    new_quest = quest
+    new_var_entry = [new_var_code, new_quest]
+    df_extended = pd.DataFrame(new_var_entry).T  # преобразовываем список во временныый датафрейм из одной строки (такая технология сейчас в pandas)
+    df_extended.columns = ['var', 'quest']
+    df_vars = pd.concat([df_vars, df_extended])  # добавляем временный датафрейм к основному датафрейму
+    with pd.ExcelWriter(vars_path, mode='a', if_sheet_exists='replace') as writer:
+        df_vars.to_excel(writer, index=False)
+
     keyboard = [
         [
             InlineKeyboardButton("в меню", callback_data='menu'),
@@ -137,9 +150,19 @@ def get_passings(update, context):
     user_id = update.effective_user.id
     uniq_id = update.message.text
 
-    with open("users.json", encoding="UTF-8") as file:
-        data = json.load(file)
-    var = data["vars"].get(uniq_id)
+    # получение прохождений
+    kod = uniq_id  # интересующий нас код
+    df_passings = pd.read_excel(passings_path, index_col=None).reset_index(drop=True)  # открываем файл с результатами
+    df_passings_filtered = df_passings[(df_passings['var'] == kod)]  # вытаскиваем строки с нашим кодом
+    quest = df_passings_filtered.iloc[0][['quest']].squeeze()  # номер задания у всех один, вытаскиваем его из первой строки
+    df_out = df_passings_filtered[['user', 'time', 'task', 'answs']].copy()  # формируем таблицу выводных данных из строк
+
+    var = {}
+    for d in df_out.values.tolist():
+        if not var.get(d[0]):
+            var[d[0]] = {"dtm": d[1], "qst": {}}
+        var[d[0]]["qst"][d[2]] = d[3]
+
     if not var:
         keyboard = [
             [
@@ -155,30 +178,36 @@ def get_passings(update, context):
         return "GET_PASSINGS"
 
     update.effective_message.reply_text(
-        text=f"""Вариант: задание {var["quest"]}""",
+        text=f"""Вариант: задание {quest}""",
         parse_mode=ParseMode.HTML
     )
-
-    for passing in filter(lambda x: var["passings"][x]["completed"], list(var["passings"].keys())):
+    for usr in var:
+        passing = var[usr]
         pass_time = ""
-        timex = var["passings"][passing]["time"]
+
+        timex = passing["dtm"]
         if timex >= 3600:
             pass_time += f"{timex//3600} ч. "
         pass_time += f"{int(timex//60%60)} мин."
         if timex < 3600:
             pass_time += f" {int(timex % 60)} сек. "
-        text = f"""Прохождение @{passing}\nВремя прохождения: {pass_time}\nОтветы:\n"""
-        for task in var["passings"][passing]["answs"]:
+
+        text = f"""Прохождение @{usr}\nВремя прохождения: {pass_time}\nОтветы:\n"""
+
+        res = 0
+        for task in passing["qst"]:
             text += f"{task}. "
-            text += "Верно" if var["passings"][passing]["answs"][task] else "Ошибка"
+            text += "Верно" if passing["qst"][task] else "Ошибка"
             text += "\n"
-        a = var["passings"][passing]["res"]
-        count = len(var['passings'][passing]['answs'])
-        text += f"Результат прохождения: {a}/{count}, {int(a/count*100)}%"
+            res += 1 if passing["qst"][task] else 0
+
+        count = len(passing["qst"])
+        text += f"Результат прохождения: {res}/{count}, {int(res/count*100)}%"
         update.effective_message.reply_text(
             text=text,
             parse_mode=ParseMode.HTML
         )
+
     keyboard = [
         [
             InlineKeyboardButton("в меню", callback_data='menu'),
@@ -187,7 +216,7 @@ def get_passings(update, context):
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.effective_message.reply_text(
-        text=f"""Всего прохождений: {len(var['passings'])}""",
+        text=f"""Всего прохождений: {len(var)}""",
         reply_markup=reply_markup,
         parse_mode=ParseMode.HTML
     )
@@ -210,10 +239,13 @@ def start_test(update, context):
     query = update.callback_query
     user_id = update.effective_user.id
     uniq_id = update.message.text
-    with open("users.json", encoding="UTF-8") as file:
-        data = json.load(file)
-    var = data["vars"].get(uniq_id)
-    if not var:
+
+    df_vars = pd.read_excel(vars_path, index_col=None).reset_index(drop=True)
+
+    var_code = uniq_id
+    var_code_entry = df_vars[(df_vars['var'] == var_code)]
+    quest = var_code_entry[['quest']].squeeze()
+    if not quest:
         keyboard = [
             [
                 InlineKeyboardButton("в меню", callback_data='menu'),
@@ -234,16 +266,17 @@ def start_test(update, context):
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
         update.effective_message.reply_text(
-            text=f"""Задание {var["quest"]}\nВ качестве ответов вписывайте числа, без пробелов, дробные числа записывайте через точку\nПример ответа: -8.35""",
+            text=f"""Задание {quest}\nВ качестве ответов вписывайте числа, без пробелов, дробные числа записывайте через точку\nПример ответа: -8.35""",
             reply_markup=reply_markup,
             parse_mode=ParseMode.HTML
         )
-        context.user_data["var"] = var
+        context.user_data["quest"] = quest
         context.user_data["uniq_id"] = uniq_id
     return "START_TEST"
 
 
 def take_test(update, context):
+    global df_passings
     query = update.callback_query
     user_id = update.effective_user.id
     username = update.effective_user.username
@@ -251,45 +284,66 @@ def take_test(update, context):
         username = str(user_id)
     amount = 10
 
-    quest = str(context.user_data["var"]["quest"])
+    quest = str(context.user_data["quest"])
 
-    # numb = context.user_data.get("numb")
+    df_questions = pd.read_excel(questions_path, index_col=None).reset_index(drop=True)
+    df_passings = pd.read_excel(passings_path, index_col=None).reset_index(drop=True)
 
     if context.user_data.get("numb"):
         text = update.message.text
         with open("answers.json", encoding="UTF-8") as file:
             data = json.load(file)
-        ans = data["quests"][quest][context.user_data["tasks"][context.user_data["numb"]-1]-1] == text
+        true_ans = df_questions.loc[((df_questions['quest'] == float(quest)) & (df_questions['task'] == float(context.user_data["tasks"][context.user_data["numb"]-1])))][['true_ans']].squeeze()
+        ans = float(text) == true_ans
         if ans:
             update.effective_message.reply_text(
                 text=f"""Верно!""",
                 parse_mode=ParseMode.HTML
             )
+            context.user_data["res"] += 1
         else:
+            if int(true_ans) == true_ans:
+                true_ans = int(true_ans)
             update.effective_message.reply_text(
-                text=f"""Неверно. Правильный ответ: {data["quests"][quest][context.user_data["tasks"][context.user_data["numb"]-1]-1]}""",
+                text=f"""Неверно. Правильный ответ: {true_ans}""",
                 parse_mode=ParseMode.HTML
             )
-        # print(data["quests"][quest][context.user_data["tasks"][context.user_data["numb"]-1]-1], text)
-        with open("users.json", "r+", encoding="UTF-8") as file:
-            data = json.load(file)
-            data["vars"][context.user_data["uniq_id"]]["passings"][username]["answs"][context.user_data["tasks"][context.user_data["numb"]-1]] = ans
-            file.seek(0)
-            file.write(json.dumps(data))
+
+        # записать правильность ответа ученика по i-ому вопросу в файл Passings.xlxs
+        passing_time = 0
+        var_code = context.user_data["uniq_id"]
+        passing_completed = False
+        user = username
+        quest_number = quest
+        task_number = context.user_data["tasks"][context.user_data["numb"]-1]
+        quest_answ = ans
+
+        new_passing_entry = [  # записываем эти значения в список
+            passing_time,
+            var_code,
+            passing_completed,
+            user,
+            quest_number,
+            task_number,
+            quest_answ
+        ]
+
+        df_extended = pd.DataFrame(new_passing_entry).T  # преобразовываем список во временныый датафрейм из одной строки (такая технология сейчас в pandas)
+        df_extended.columns = ['time', 'var', 'completed', 'user', 'quest', 'task', 'answs']
+        df_passings = pd.concat([df_passings, df_extended])  # добавляем временный датафрейм к основному датафрейму
+        with pd.ExcelWriter(passings_path, mode='a', if_sheet_exists='replace') as writer:  # записываем дополненный датафрейм в исходный файл
+            df_passings.to_excel(writer, index=False)
     else:
         context.user_data["numb"] = 0
+        context.user_data["res"] = 0
         with open("answers.json", encoding="UTF-8") as file:
             data = json.load(file)
         context.user_data["tasks"] = []
+        mx = len(df_questions.loc[(df_questions['quest'] == context.user_data["quest"])].axes[0])
         while len(context.user_data["tasks"]) < amount:
-            n = r.randint(1, len(data["quests"][quest]))
+            n = r.randint(1, mx)
             if n not in context.user_data["tasks"]: context.user_data["tasks"].append(n)
         context.user_data["tasks"].sort()
-        with open("users.json", "r+", encoding="UTF-8") as file:
-            data = json.load(file)
-            data["vars"][context.user_data["uniq_id"]]["passings"][username] = {"completed": False, "answs": {}}
-            file.seek(0)
-            file.write(json.dumps(data))
         context.user_data["start_time"] = time.time()
 
     if context.user_data["numb"] < amount:
@@ -307,26 +361,15 @@ def take_test(update, context):
                 parse_mode=ParseMode.HTML
             )
     else:
-        with open("users.json", "r", encoding="UTF-8") as file:
-            data = json.load(file)
-            data["vars"][context.user_data["uniq_id"]]["passings"][username]["completed"] = True
-            a = list(data["vars"][context.user_data["uniq_id"]]["passings"][username]["answs"].values()).count(True)
-            data["vars"][context.user_data["uniq_id"]]["passings"][username]["res"] = a
-            data["vars"][context.user_data["uniq_id"]]["passings"][username]["time"] = time.time() - context.user_data["start_time"]
-        with open("users.json", "w", encoding="UTF-8") as file:
-            file.seek(0)
-            file.write(json.dumps(data))
-        # a = list(data["vars"][context.user_data["uniq_id"]]["passings"][username]["answs"].values()).count(True)
         keyboard = [[InlineKeyboardButton("В меню", callback_data='menu')]]
         reply_markup = InlineKeyboardMarkup(keyboard)
         update.effective_message.reply_text(
-            text=f"""{a}/{amount}""",
+            text=f"""{context.user_data["res"]}/{amount}""",
             reply_markup=reply_markup,
             parse_mode=ParseMode.HTML
         )
 
         del context.user_data["numb"]
-        del context.user_data["var"]
         del context.user_data["uniq_id"]
 
     return "TAKE_TEST"
